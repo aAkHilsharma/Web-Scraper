e3bca539bde2cd22a83e1a27b4eb9d66b83f2ead
aAkHilsharma <akhilsharma6721@gmail.com>
2023-08-15 04:52:25 -0700

    deployment fix

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "index.html"));
   });
 }
 
