c5def317eab214a12794718ab9a5411fe49783bb
aAkHilsharma <akhilsharma6721@gmail.com>
2023-08-15 05:21:25 -0700

    small changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const path = require("path");
 const app = express();
 require("./config/dbConfig");
 const cors = require("cors");
@@ -10,22 +11,19 @@ app.use(cors());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/scrape", require("./routes/scrape"));
 
-//for deployment
+// Configure paths for deployment
 const rootPath = path.resolve(__dirname);
 const clientBuildPath = path.join(rootPath, "server", "client", "build");
 
-const path = require("path");
-__dirname = path.resolve();
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(clientBuildPath)));
+  app.use(express.static(clientBuildPath));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(clientBuildPath, "index.html"));
   });
 }
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
